6e3bf0ace1959e0c35cbe852c791054c0eeed2a4
theoreotm <qwertzybad@gmail.com>
2024-03-12 01:06:51 +0500

    feat: some changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,24 @@
 	import { page } from '$app/stores';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
+	// Highlight.js
+	import hljs from 'highlight.js/lib/core';
+
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import json from 'highlight.js/lib/languages/json';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+	import shell from 'highlight.js/lib/languages/shell';
+
+	// Register each imported language module
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('json', json);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	hljs.registerLanguage('shell', shell);
+
 	import PageTransition from '$lib/components/PageTransition.svelte';
 
 	$: isActivePath = (path: string) => {
